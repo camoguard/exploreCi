7dc2b1c74f027f5b3ec2a8a1de23063cc13a6e1e
Camoguard <camoguard@gmail.com>
2023-04-30 16:37:05 -0500

    breaking change

--- vsRoot/ciTestConsoleApp/ConsoleApp1/Behavior.cs
+++ vsRoot/ciTestConsoleApp/ConsoleApp1/Behavior.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         public string ConcatenateThreeStrings(string in1, string in2, string in3)
         {
-            var sb = new StringBuilder(in1 + in2 + in3);
+            var sb = new StringBuilder(in1 + in2 + in3 + "12");
             return sb.ToString();
         }
     }
